6e33b14893db2fdf774996f3dc39b2bb49e336af
leon <1506816370@qq.com>
2020-12-06 18:15:32 +0800

    1、ASP.NET网页立即过期

---
+++ WebApplication1/Controllers/ExpireController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ExpireController : Controller
+    {
+        // GET: Expire
+        public ActionResult Index()
+        {
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            Response.Expires = 0;
+            Response.ExpiresAbsolute = DateTime.Now.AddDays(-1);
+            Response.AddHeader("progma", "no-cache");
+            Response.AddHeader("cache-control", "private");
+            Response.CacheControl = "no-cache";
+
+            ViewBag.Title = DateTime.Now.ToString("mm:ss");
+
+            return View();
+        }
+    }
+}
